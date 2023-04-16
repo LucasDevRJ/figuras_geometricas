7595e4a0c2bec8897cc7a7059b9aa06d1c3ac5f2
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-04-16 17:48:47 -0300

    Adição do CSS

--- javaScript/index.js
+++ javaScript/index.js
@@ -4,6 +4,8 @@ function desenhaFiguraGeometrica() {
 	var campoDesenho = document.getElementById("campo-desenho");
 
 	var tamanho = parseInt(campoTamanho.value);
+	var desenho;
+	var br;
 
 	campoDesenho = document.body;
 
@@ -11,16 +13,30 @@ function desenhaFiguraGeometrica() {
 
 	for (var i = 0; i < figurasGeometricas.length; i++) {
 		if (figurasGeometricas[i].checked) {
-			if (figurasGeometricas[i].value == "triangulo") {
+			if (figurasGeometricas[i].value == "quadrado") {
 				for (var linha = 0; linha < tamanho; linha++) {
 					for (var coluna = 0; coluna < tamanho; coluna++) {
-						var desenho  = document.createTextNode("*");
+						desenho  = document.createTextNode("*");
 						campoDesenho.appendChild(desenho);
 					}
-					var br = document.createElement('br');
+					br = document.createElement('br');
 					campoDesenho.appendChild(br);
 				}
-			} 
-		}  
+			} else {
+				console.log("triangulo")
+				for (var linha = 0; linha < tamanho; linha++) {
+					for (var coluna = 0; coluna < tamanho; coluna++) {
+						if (coluna < linha) {
+							desenho  = document.createTextNode("*");
+							campoDesenho.appendChild(desenho);
+						}
+					}
+					br = document.createElement('br');
+					campoDesenho.appendChild(br);
+				}
+			}
+		} else {
+			console.log("Escolha alguma figura");
+		} 
 	} 
 }
