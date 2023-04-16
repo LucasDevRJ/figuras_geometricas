e60b510c503ba13c988193e9ed474807e5a2560f
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-04-16 16:16:57 -0300

    Criação da lógica para desenhar quadrado

--- javaScript/index.js
+++ javaScript/index.js
@@ -1,16 +1,24 @@
 function desenhaFiguraGeometrica() {
 	var figurasGeometricas = document.getElementsByName("figura-geometrica");
 	var campoTamanho = document.getElementById("tamanho");
-	var campoDesenho = document.getElementById("desenho");
-	var tamanho = campoTamanho.value;
+	var campoDesenho = document.getElementById("campo-desenho");
+
+	var tamanho = parseInt(campoTamanho.value);
+
+	campoDesenho = document.body;
+
+	console.log(campoDesenho);
 
 	for (var i = 0; i < figurasGeometricas.length; i++) {
 		if (figurasGeometricas[i].checked) {
 			if (figurasGeometricas[i].value == "triangulo") {
 				for (var linha = 0; linha < tamanho; linha++) {
 					for (var coluna = 0; coluna < tamanho; coluna++) {
-
+						var desenho  = document.createTextNode("*");
+						campoDesenho.appendChild(desenho);
 					}
+					var br = document.createElement('br');
+					campoDesenho.appendChild(br);
 				}
 			} 
 		}  
