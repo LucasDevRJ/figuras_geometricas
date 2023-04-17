6f01c5e3a3c3449020da38e2e5de77c9600049c8
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-04-16 21:32:20 -0300

    Estilização do espaço das figuras

--- javaScript/index.js
+++ javaScript/index.js
@@ -3,35 +3,39 @@ function desenhaFiguraGeometrica() {
 	var campoTamanho = document.getElementById("tamanho");
 	var campoDesenho = document.getElementById("campo-desenho");
 
+	// Criar elemento
+	var p = document.createElement('p');
+	p.style.color = "#FFFFFF";
+	p.style.display = "flex";
+	p.style.alignItems = "center";
+	p.style.justifyContent = "center";
+	p.style.backgroundColor = "#000000";
+	p.style.padding = "30px 50px";
+
 	var tamanho = parseInt(campoTamanho.value);
-	var desenho;
-	var br;
-
-	campoDesenho = document.body;
-
-	console.log(campoDesenho);
 
 	for (var i = 0; i < figurasGeometricas.length; i++) {
 		if (figurasGeometricas[i].checked) {
 			if (figurasGeometricas[i].value == "quadrado") {
 				for (var linha = 0; linha < tamanho; linha++) {
 					for (var coluna = 0; coluna < tamanho; coluna++) {
-						desenho  = document.createTextNode("*");
-						campoDesenho.appendChild(desenho);
+						var desenho  = document.createTextNode("*");
+						p.appendChild(desenho);
+						campoDesenho.appendChild(p);
 					}
-					br = document.createElement('br');
-					campoDesenho.appendChild(br);
+					var br = document.createElement('br');
+					p.appendChild(br);
 				}
 			} else {
 				console.log("triangulo")
 				for (var linha = 0; linha < tamanho; linha++) {
 					for (var coluna = 0; coluna < tamanho; coluna++) {
 						if (coluna < linha) {
-							desenho  = document.createTextNode("*");
+							var desenho  = document.createTextNode("*");
 							campoDesenho.appendChild(desenho);
 						}
 					}
-					br = document.createElement('br');
+					var br = document.createElement('br');
 					campoDesenho.appendChild(br);
 				}
 			}
